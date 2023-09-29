64c571d45913855ee7a7562ebafea6c1b05d95e7
Arnol Cáceres <116393375+arnolcb@users.noreply.github.com>
2023-09-28 19:09:40 -0500

    feature: delete comments

--- app/src/main/java/com/example/android/front_cyclescape/controllers/ShoppingActivity.kt
+++ app/src/main/java/com/example/android/front_cyclescape/controllers/ShoppingActivity.kt
@@ -13,32 +13,24 @@ class ShoppingActivity : AppCompatActivity() {
 
     private lateinit var precioTotalTextView: TextView
     private lateinit var mensajeTextView: TextView
-    private lateinit var carritoRecyclerView: RecyclerView
     private lateinit var siguienteProcesoButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
 
-        // Inicializar las vistas
         precioTotalTextView = findViewById(R.id.precio_total)
         mensajeTextView = findViewById(R.id.mensaje1)
-        //carritoRecyclerView = findViewById(R.id.carrito_lista)
         siguienteProcesoButton = findViewById(R.id.siguiente_proceso)
 
-        // Configurar el comportamiento del botón
         siguienteProcesoButton.setOnClickListener {
-            // Aquí puedes agregar lógica para continuar con el siguiente proceso
         }
 
-        // Actualizar el texto del precio total (deberías hacerlo cuando se agreguen elementos al carrito)
-        actualizarPrecioTotal(0.0) // Inicialmente, el precio total es $0
+        actualizarPrecioTotal(0.0)
     }
 
     private fun actualizarPrecioTotal(total: Double) {
         precioTotalTextView.text = "Total: $$total"
     }
-
-    // Agrega más funciones según tus necesidades
 
 }
