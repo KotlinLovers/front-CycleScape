1ed3b2e3fb62f21227478efb29ecc57d26378de1
FabrizCastro <fabrizziocastro2612@gmail.com>
2023-09-28 19:45:04 -0500

    Add activitys (Signup and Login)

---
+++ app/src/main/java/com/example/android/front_cyclescape/controllers/RegistrationData.kt
@@ -0,0 +1,11 @@
+package com.example.android.front_cyclescape.controllers
+
+data class RegistrationData(
+    val userFirstName: String,
+    val userLastName: String,
+    val userEmail: String,
+    val userPassword: String,
+    val userPhone: String,
+    val userBirthDate: String,
+    val imageData: String
+)
